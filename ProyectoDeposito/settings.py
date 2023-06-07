145a9e2760826d9ac255f13ed33414088d258269
jfdaza <mainekko23@gmail.com>
2023-06-07 12:11:09 -0300

    Final 1.1.3

--- ProyectoDeposito/settings.py
+++ ProyectoDeposito/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.74','127.0.0.1, 192.168.0.116']
+ALLOWED_HOSTS = ['192.168.0.74','127.0.0.1', '192.168.0.116']
 
 
 # Application definition
